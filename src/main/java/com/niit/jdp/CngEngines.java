da69f05213dcf926f3b0bd3ac199afdc22d38f8c
Mohit Saini <mohit.saini09868@gmail.com>
2022-08-30 01:14:26 +0530

    define a attributes in variable in CngEngines.java class

---
+++ src/main/java/com/niit/jdp/CngEngines.java
@@ -0,0 +1,11 @@
+/*
+ * Author Name: Mohit Saini
+ * Date: 29-08-2022
+ * Created With: IntelliJ IDEA Community Edition
+ */
+package com.niit.jdp;
+
+public class CngEngines extends Engines {
+    private String typeOfCNG;
+    
+}
