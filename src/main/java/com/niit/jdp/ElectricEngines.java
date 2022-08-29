5783ad4e544cf1bce101b87c5ee64685cf284a56
Mohit Saini <mohit.saini09868@gmail.com>
2022-08-30 01:01:11 +0530

    Define a Constructor and super class in ElectricEngines.java class

--- src/main/java/com/niit/jdp/ElectricEngines.java
+++ src/main/java/com/niit/jdp/ElectricEngines.java
@@ -7,4 +7,14 @@ package com.niit.jdp;
 
 public class ElectricEngines extends Engines {
     private double voltages;
+    
+    public ElectricEngines ( double voltages ) {
+        this.voltages = voltages;
+    }
+    
+    public ElectricEngines ( String modelNumber , double maxPower , double maxRpm , double voltages ) {
+        super ( modelNumber , maxPower , maxRpm );
+        this.voltages = voltages;
+    }
+    
 }
