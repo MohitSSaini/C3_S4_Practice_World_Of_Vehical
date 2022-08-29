56a6732a0f7f8f5d25b9054654bc27f7c49f8b1b
Mohit Saini <mohit.saini09868@gmail.com>
2022-08-30 01:00:16 +0530

    Define a attributes and variables in ElectricEngines.java class

---
+++ src/main/java/com/niit/jdp/ElectricEngines.java
@@ -0,0 +1,10 @@
+/*
+ * Author Name: Mohit Saini
+ * Date: 29-08-2022
+ * Created With: IntelliJ IDEA Community Edition
+ */
+package com.niit.jdp;
+
+public class ElectricEngines extends Engines {
+    private double voltages;
+}
