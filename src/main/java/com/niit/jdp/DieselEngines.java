35ed9424df8a8ca19294607439478487707fa403
Mohit Saini <mohit.saini09868@gmail.com>
2022-08-30 01:11:07 +0530

    create a constructor and super class  in DieselEngines.java class

--- src/main/java/com/niit/jdp/DieselEngines.java
+++ src/main/java/com/niit/jdp/DieselEngines.java
@@ -8,4 +8,13 @@ package com.niit.jdp;
 public class DieselEngines extends Engines {
     private String typeOfFuel;
     
+    public DieselEngines ( String typeOfFuel ) {
+        this.typeOfFuel = typeOfFuel;
+    }
+    
+    public DieselEngines ( String modelNumber , double maxPower , double maxRpm , String typeOfFuel ) {
+        super ( modelNumber , maxPower , maxRpm );
+        this.typeOfFuel = typeOfFuel;
+    }
+    
 }
