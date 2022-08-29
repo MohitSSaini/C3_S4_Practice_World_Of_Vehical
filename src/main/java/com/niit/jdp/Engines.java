6b5e3371fc99170401530ff312ece701a67aeb0e
Mohit Saini <mohit.saini09868@gmail.com>
2022-08-30 00:50:35 +0530

    Define attributes variable in Engines.java class

---
+++ src/main/java/com/niit/jdp/Engines.java
@@ -0,0 +1,13 @@
+/*
+ * Author Name: Mohit Saini
+ * Date: 29-08-2022
+ * Created With: IntelliJ IDEA Community Edition
+ */
+package com.niit.jdp;
+
+public class Engines {
+    private String modelNumber;
+    private double maxPower;
+    private double maxRpm;
+    
+}
