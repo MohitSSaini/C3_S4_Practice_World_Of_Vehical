595f1440ebc67a475ef67276f2d397b5ce29054c
Mohit Saini <mohit.saini09868@gmail.com>
2022-08-30 01:08:56 +0530

    Define attributes and variables in DieselEngines.java class

---
+++ src/main/java/com/niit/jdp/DieselEngines.java
@@ -0,0 +1,11 @@
+/*
+ * Author Name: Mohit Saini
+ * Date: 29-08-2022
+ * Created With: IntelliJ IDEA Community Edition
+ */
+package com.niit.jdp;
+
+public class DieselEngines extends Engines {
+    private String typeOfFuel;
+    
+}
