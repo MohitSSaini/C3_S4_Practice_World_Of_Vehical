b7668fb9cf4ff029be4455cf022ea49eb2619ae2
Mohit Saini <mohit.saini09868@gmail.com>
2022-08-30 00:54:31 +0530

    Define a attributes variable in PetrolEngines.java class

---
+++ src/main/java/com/niit/jdp/PetrolEngines.java
@@ -0,0 +1,13 @@
+/*
+ * Author Name: Mohit Saini
+ * Date: 29-08-2022
+ * Created With: IntelliJ IDEA Community Edition
+ */
+package com.niit.jdp;
+
+public class PetrolEngines extends Engines {
+    private double displacement;
+    private int noOfCylinder;
+    
+    
+}
